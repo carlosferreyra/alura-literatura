df617d2e0f62d00f917678de99346b9f99d64d87
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2024-10-28 13:45:29 -0300

    Delete literatura directory

--- literatura/src/main/java/com/alura/literatura/repository/AutorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alura.literatura.repository;
-
-import com.alura.literatura.model.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutorRepository extends JpaRepository<Autor, Long> {
-}
